773dfd90c588bea4ed519bf2c0cf1004f7b5a501
Zanneta <janet.tafiy@gmail.com>
2022-09-13 00:22:53 +0300

    Lesson 1 - Added 4 new methods (printThreeWords, checkSumSign, printColor, compareNumbers)

--- AQA/src/main/java/course/HomeWorkApp.java
+++ AQA/src/main/java/course/HomeWorkApp.java
@@ -1,8 +1,61 @@
 package course;
 
+import java.util.Random;
+import java.util.Scanner;
 
-public class HomeWorkApp {
-    public static void main( String[] args ) {
-
+public class HomeWorkApp{
+    public static void main( String[] args ){
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+    public static void printThreeWords(){
+        String orange = "Orange";
+        String banana = "Banana";
+        String apple = "Apple";
+        System.out.println(orange);
+        System.out.println(banana);
+        System.out.println(apple);
+    }
+    public static void checkSumSign(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number");
+        int a = scanner.nextInt();
+        System.out.println("Enter second number");
+        int b = scanner.nextInt();
+        int sum = a+b;
+        if(sum >= 0){
+            System.out.println("Sum is positive");
+        }
+        else{
+            System.out.println("Sum is negative");
+        }
+    }
+    public static void printColor(){
+        Random rand = new Random();
+        int value = rand.nextInt(301) - 100;
+        if(value <= 0){
+            System.out.println("Red");
+        }
+        else if(value <= 100){
+            System.out.println("Yellow");
+        }
+        else{
+            System.out.println("Green");
+        }
+    }
+    public static void compareNumbers(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number");
+        int a = scanner.nextInt();
+        System.out.println("Enter second number");
+        int b = scanner.nextInt();
+        if(a >= b){
+            System.out.println("a >= b");
+        }
+        else{
+            System.out.println("a < b");
+        }
     }
 }
